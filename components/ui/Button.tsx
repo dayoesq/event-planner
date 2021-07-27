3f372ccd300acc5b12582ccd97d42d834f84a936
Dayo <dayoesq@live.com>
2021-07-27 15:51:01 +0300

    EventSearch component added

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -3,14 +3,23 @@ import classes from './Button.module.css';
 
 
 type ButtonProps = {
-  link: string;
+  link?: string;
   children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 const Button: React.FC<ButtonProps> = props => {
-  return <Link href={props.link}>
-    <a className={classes.btn}>{props.children}</a>
-  </Link>;
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  } else {
+    return (
+      <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
+    );
+  }
 };
 
 export default Button;
