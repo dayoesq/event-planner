b28091ed1148709578498191a080283d50740536
Dayo <dayoesq@live.com>
2021-07-27 12:17:23 +0300

    Added icons

--- components/EventItem.tsx
+++ components/EventItem.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image';
 import { IItem } from './EventList';
 
 import classes from './EventItem.module.css';
-import Button from './ui/button';
+import Button from './ui/Button';
+import DateIcon from './icons/date-icon';
+import AddressIcon from './icons/address-icon';
+import ArrowRightIcon from './icons/arrow-right-icon';
 
 
 const EventItem: React.FC<IItem> = props => {
@@ -23,14 +26,19 @@ const EventItem: React.FC<IItem> = props => {
         <div className={classes.summary}>
           <h1>{props.title}</h1>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={`/events/${props.id}`}>Explore Events</Button>
+          <Button link={`/events/${props.id}`}>
+            <span>Explore Events</span>
+            <span className={classes.icon}><ArrowRightIcon /></span>
+          </Button>
         </div>
       </div>
     </li>
